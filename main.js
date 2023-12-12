0ca9b19b74517ebd74e67d164a24568a3c4d3a55
XxMGxX IT/AI <mohamad7719603@gmail.com>
2023-12-12 07:31:00 +0200

    Add some featuers to the Game.

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-// Error[1]: What if the same question appers?
-// Error[2]: How we will make more than question in the same test
+// Error[1]: What if the same question appers => (Solved)
+// Error[2]: How we will make more than question in the same test 
+// Error[3]: Fix the changeQ function on line (49) to change the answer lines like the question.
 
 let sec = document.getElementById("sec")
 let cube = document.querySelectorAll(".cube")
 let Q = document.querySelector(".question h1")
 let lines = document.querySelector(".lines")
 let msg = document.getElementById("msg")
-
-
+let game_over = document.getElementById("gameOver")
+let duration = document.querySelector(".timer span")
+duration.innerHTML = 30
 
 
 let questions = ["ما هي عاصمة مصر؟",
 "ما هو اقرب كوكب الى الشمس؟", "ما هو اطول برج في العالم؟",
 "ما هو الكوكب الاشد حرارة في المجموعة الشمسية؟", "اين تقع اليابان؟", "ما هو اسم مبرمج هذه اللعبة؟",
 "ما هي عاصمة فلسطين؟"]
-let answers = ["القاهرة", "عطارد", "خليفة", "الزهرة", "اسيا", "محمد", "غزة"]
+let answers = ["القاهرة", "عطارد", "خليفة", "الزهرة", "اسيا", "محمد", "القدس"]
 let Q_A_num = Math.floor(Math.random() * questions.length)
+
+
+
+// Returns a shuffled array
+let newArray = []
+for(let i = 0; i < questions.length; i++){
+    newArray.push(i)
+}
+function shufflingArray(){
+    for(let i = questions.length - 1; i > 0; i--){
+        n = Math.floor(Math.random() * (i + 1))
+        let snum = newArray[i]
+        newArray[i] = newArray[n]
+        newArray[n] = snum
+    }
+    console.log(newArray)
+}
+
 
 Q.innerHTML = questions[Q_A_num]
 
@@ -26,6 +46,22 @@ for(let i = 0; i < answers[Q_A_num].length; i++){
 }
 
 let line = document.querySelectorAll(".lines .line")
+
+let cou = 0
+function changeQ(){
+    Q.innerHTML = questions[newArray[cou]]
+
+for(let i = 0; i < answers[Q_A_num].length; i++){
+    let div = document.createElement("div")
+    div.classList.add("line")
+    lines.append(div)
+}
+cou += 1
+}
+
+
+
+
 
 
 // Disappering function
@@ -53,6 +89,8 @@ function Appering(id){
 // Counter
 let num = 0
 
+let count = setInterval(timer, 1000)
+
 // Right cube choosen
 cube.forEach(function(e){
     // Adding click event for each cube
@@ -69,6 +107,7 @@ cube.forEach(function(e){
                     Appering(msg)
                 }, 2000)
             }
+            
             // Empty array for save lines
             let arr = []
             // If words = answer length
@@ -116,3 +155,26 @@ cube.forEach(function(e){
         }
     })
 })
+
+
+
+// Timer
+function timer(){
+    duration.innerHTML -= 1 
+    if(duration.innerHTML === "10"){
+        document.querySelector(".timer").style.color = "red"
+    }
+    if(num === answers[Q_A_num].length){
+        clearInterval(count)
+    }
+    if(duration.innerHTML === "0"){
+        clearInterval(count)
+        document.getElementById("boo").play()
+        setTimeout(() =>{
+            Disappering(sec)
+        }, 1000)
+        setTimeout(() =>{
+            Appering(game_over)
+        }, 1500)
+    }
+}
